c5f54f88049f233bc592af8ff6b3d7b7bf8639eb
shenghanqin <shenghanqin@hujiang.com>
2020-04-25 16:19:36 +0800

    update 官网继续编写，剩下中间内容

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Route, Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import Home from './pages/home';
@@ -8,14 +8,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router history={createBrowserHistory({ basename: process.env.PUBLIC_URL })}>
-          <div className="route">
-            {/* <Header /> */}
-            <Route exact path="/" component={Home} />
-          </div>
-        </Router>
-      </div>
+      <Router history={createBrowserHistory({ basename: process.env.PUBLIC_URL })}>
+        <Fragment className="route">
+          {/* <Header /> */}
+          <Route exact path="/" component={Home} />
+        </Fragment>
+      </Router>
+      // <div className="App">
+      // </div>
     );
   }
 }

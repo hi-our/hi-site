7a7678804a7176f44cab8cdcbab4b1250d96e64c
shenghanqin <shenghanqin@hujiang.com>
2020-04-25 15:25:00 +0800

    update 更新reset css

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Page from '../components/page';
 
 
-import './home.css';
+import './styles.css';
 
 export default class Home extends Component {
   render() {
@@ -13,14 +13,32 @@ export default class Home extends Component {
       >
         <header>
           <div className="header-main">
-            <a className="logo"></a>
-            <nav></nav>
-
+            <a href='/' className="logo">
+              <h1>Hi Our</h1>
+            </a>
+            <nav className="main-nav">
+              <a href='/' title="Hi Our">首页</a>
+              <a href='/hi-face' title="Hi头像教程">头像小册</a>
+            </nav>
           </div>
         </header>
         <section className="feature-1"></section>
         <footer className="footer">
-          底部模块
+          <div className="footer-main">
+            <a href='/' className="footer-logo">
+              <h3>Hi Our</h3>
+            </a>
+            <section className="footer-links">
+              <dl>
+                <dt>关于我们</dt>
+                <dd>Hi Our</dd>
+                <dd>加入我们</dd>
+                <dd>联系我们</dd>
+              </dl>
+
+            </section>
+          </div>
+          
         </footer>
       </Page>
     );

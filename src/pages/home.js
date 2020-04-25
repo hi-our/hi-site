6383ce9fc068c4180e09894c55a2baa85b7ba447
shenghanqin <shenghanqin@hujiang.com>
2020-04-25 18:29:53 +0800

    update 初步手机适配

--- src/pages/home.js
+++ src/pages/home.js
@@ -26,21 +26,21 @@ export default class Home extends Component {
         <div className="page-wrap">
           <div className="page-main">
             <section className="hiface-wrap">
-              <div>
+              <div className="hiface-main">
+                <div className="hiface-logo">
+                  <img src={require('../img/hiface-logo.png')} alt="Hi头像" />
+                </div>
                 <h2>Hi头像</h2>
                 <p>记录生活的点滴</p>
-                <div className="button-wrap">
-                  <div className="button-try">
-                    快速体验
-                    <div className="qrcode-mask">
-                      <img className="hiface-qrcode" src={require('../img/hiface-qrcode.jpg')} alt="Hi头像小程序码" />
-                    </div>
-                  </div>
-                  <a className="button-github" href="https://github.com/hi-our/hi-face">GitHub</a>
-                </div>
               </div>
-              <div className="hiface-logo">
-                <img src={require('../img/hiface-logo.png')} alt="Hi头像" />
+              <div className="button-wrap">
+                <div className="button-try">
+                  快速体验
+                  <div className="qrcode-mask">
+                    <img className="hiface-qrcode" src={require('../img/hiface-qrcode.jpg')} alt="Hi头像小程序码" />
+                  </div>
+                </div>
+                <a className="button-github" href="https://github.com/hi-our/hi-face">GitHub</a>
               </div>
               
             </section>

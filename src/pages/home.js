4c52d2e4e056591b82a8d133a209c1a8b13c421d
shenghanqin <shenghanqin@hujiang.com>
2020-04-25 16:10:13 +0800

    update 更新logo，底部等

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Page from '../components/page';
+import { Link } from 'react-router-dom';
 
 
 import './styles.css';
@@ -13,9 +14,9 @@ export default class Home extends Component {
       >
         <header>
           <div className="header-main">
-            <a href='/' className="logo">
+            <Link to='/' className="logo">
               <h1>Hi Our</h1>
-            </a>
+            </Link>
             <nav className="main-nav">
               <a href='/' title="Hi Our">首页</a>
               <a href='/hi-face' title="Hi头像教程">头像小册</a>
@@ -30,12 +31,26 @@ export default class Home extends Component {
             </a>
             <section className="footer-links">
               <dl>
-                <dt>关于我们</dt>
-                <dd>Hi Our</dd>
-                <dd>加入我们</dd>
-                <dd>联系我们</dd>
+                <dt><img src={require("../img/footer-about@2x.png")} />关于我们</dt>
+                <dd><Link to="/about">Hi Our</Link></dd>
+                <dd><Link to="/about#join">加入我们</Link></dd>
+                <dd><Link to="/about#contact">联系我们</Link></dd>
               </dl>
-
+              {/* <dl>
+                <dt>社区</dt>
+                <dd>GitHub</dd>
+                <dd>微信</dd>
+              </dl> */}
+              <dl>
+                <dt><img src={require("../img/footer-resource@2x.png")} />相关资源</dt>
+                <dd><a href="https://cloudbase.net">云开发 CloudBase</a></dd>
+                <dd><a href="https://taro.jd.com">Taro</a></dd>
+              </dl>
+              <dl>
+                <dt><img src={require("../img/footer-link@2x.png")} />友情链接</dt>
+                <dd><a href="https://tuture.co">图雀社区</a></dd>
+                <dd><a href="https://xiaoxili.com">小溪里</a></dd>
+              </dl>
             </section>
           </div>
           

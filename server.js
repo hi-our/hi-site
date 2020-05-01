2d393be4ef754ba14ed3f1f482f674e1bc425ede
shenghanqin <shenghanqin@hujiang.com>
2020-05-01 21:15:53 +0800

    update 更新路由效果

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 

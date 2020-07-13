6eba12a40f890ad95f53646d57697941bc6b027c
shenghanqin <shenghanqin@163.com>
2020-07-13 09:46:09 +0800

    update 更新关于等文案

--- components/header/index.js
+++ components/header/index.js
@@ -8,14 +8,15 @@ export default class Page extends React.Component {
       <header>
         <div className="header-main">
           <h1 className="logo">
-            <Link href='/'>
+            <Link href='/' addHtml={false}>
               小溪里 xiaoxili.com
             </Link>
           </h1>
           <nav className="main-nav">
-            <Link href='/'>首页</Link>
-            <Link href='/about'>关于</Link>
+            <Link href='/' addHtml={false}>首页</Link>
+            <a href='/blog' title="小溪里博客">博客</a>
             <a href='/hi-face' title="Hi头像教程">小册</a>
+            <Link href='/about' addHtml>关于</Link>
           </nav>
         </div>
       </header>
--- components/link-html/index.js
+++ components/link-html/index.js
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 
-function NavLink({ href = '', name = '', children = '' }) {
+function NavLink({ href = '', name = '', children = '', addHtml = true }) {
   // Must add passHref to Link
   return (
-    <Link href={href + ((href != './' || href != '/')  && process.env.NODE_ENV === 'production' ? '.html' : '')} passHref>
+    <Link href={href + (addHtml  && process.env.NODE_ENV === 'production' ? '.html' : '')} passHref>
       <a>{ name || children}</a>
     </Link>
   )

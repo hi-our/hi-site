8eedb95483bb085f1044e73f0329e389f558dcfa
shenghanqin <shenghanqin@163.com>
2020-11-28 10:53:42 +0800

    update 更新友情链接

--- components/footer/index.js
+++ components/footer/index.js
@@ -17,17 +17,20 @@ export default class Footer extends React.Component {
             </dl>
             <dl>
               <dt><img src="https://image-hosting.xiaoxili.com/img/20200712164456.png" alt="相关资源" />社区</dt>
-              <dd><a href="https://cloudbase.net">云开发 CloudBase</a></dd>
+              {/* <dd><a href="https://cloudbase.net">云开发 CloudBase</a></dd> */}
               <dd><a href="https://taro.jd.com">Taro</a></dd>
               <dd><a href="https://tuture.co">图雀社区</a></dd>
             </dl>
-            {/* <dl>
-              <dt><img src={require("../../img/footer-link@2x.png")} alt="友情链接" />友情链接</dt>
-            </dl> */}
+            <dl>
+              <dt><img src="https://image-hosting.xiaoxili.com/img/20200712183726.png" alt="友情链接" />友情链接</dt>
+              <dd><a href="https://coder.itclan.cn/">itclanCoder</a></dd>
+              <dd><a href="https://www.appclip.icu/">APPCLIP</a></dd>
+            </dl>
           </section>
         </div>
         <div className="footer-copyright">
-          Copyright © 2020. All Rights Reserved.<br /><a href="http://www.beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">沪ICP备20020594号-2</a>
+          Copyright © 2020. All Rights Reserved.<br /><a href="http://www.beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">沪ICP备20020594号</a>
+          <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31010502005403" target="_blank" rel="noopener noreferrer"><img src="https://image-hosting.xiaoxili.com/img/20200812141626.png" />沪公网安备 31010502005403号</a>
         </div>
 
       </footer>

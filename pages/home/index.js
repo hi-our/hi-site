513b8177214b378c00ffbc588c8dd510256f7ed2
shenghanqin <shenghanqin@hujiang.com>
2021-01-30 17:13:10 +0800

    update 更新Banner区域

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,52 +1,15 @@
 import React, { PureComponent } from 'react'
 import Page from '../../components/page'
-import './styles.styl'
+import Banner from "./components/banner";
 
+import './styles.styl'
 export default class Home extends PureComponent {
   render() {
     const { title, pageClassName } = this.props
 
     return (
       <Page title={title} pageClassName={pageClassName}>
-        {/* 模块头部 */}
-        <section className="module-banner">
-          <div className="page-auto">
-            {/* 线条 */}
-            <div className="line"></div>
-            {/* 小程序创意动画 */}
-            <section className="animation">
-              <div className="animation-module">
-                <h3 className="animation-module-title">小程序 TabBar 创意动画</h3>
-                <p className="animation-module-text">精美小巧的动画，让你耳目一新</p>
-                <button className="animation-module-go">去看看</button>
-              </div>
-              <img className="animation-little" src="/images/little-people.png" alt="小人"/>
-            </section>
-          </div>
-          {/* 小程序创意动画类别 */}
-          <ul className="program-category page-auto">
-            <li className="program-category-item">
-              <img src="/images/notebook.png" alt="用户体验"/>
-              <h4>用户体验</h4>
-              <p>擅长响应式设计 精准还原设计稿 强调组件化开发</p>
-            </li>
-            <li className="program-category-item">
-              <img src="/images/stars.png" alt="用户体验"/>
-              <h4>用户体验</h4>
-              <p>擅长响应式设计 精准还原设计稿 强调组件化开发</p>
-            </li>
-            <li className="program-category-item">
-              <img src="/images/animation.png" alt="交互动画"/>
-              <h4>交互动画</h4>
-              <p>熟悉基础动画 创新动画专题 分享动画心得</p>
-            </li>
-            <li className="program-category-item">
-              <img src="/images/created.png" alt="开源创造"/>
-              <h4>开源创造</h4>
-              <p>参与开源组织 开源精巧组件 热心开发者群体</p>
-            </li>
-          </ul>
-        </section>
+        <Banner />
       </Page>
     )
   }

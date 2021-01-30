face2f49a99e105079b9733815dafdb9403b9514
shenghanqin <shenghanqin@hujiang.com>
2021-01-30 15:23:51 +0800

    update 第一番改进

--- components/header/index.js
+++ components/header/index.js
@@ -6,31 +6,33 @@ export default class Header extends React.Component {
   state = {
     menuState: false // 导航菜单的状态 
   }
+
   changeMenuState = state => this.setState({menuState: state})
+
+  toggleMenuStatus = () => {
+    this.setState({
+      menuState: !this.state.menuState
+    })
+  }
+
   render() {
     const {menuState} = this.state
     return (
       <>
         {/* 头部 */}
         <header className="header">
-          <div className="header-auto">
-            <h1 className="header-title">
-              <img src="/images/logo.png" alt="logo"/>
+          <div className="header-main">
+            <h1 className="logo">
+              <Link href='/' addHtml={false}>
+                小溪里 xiaoxili.com
+              </Link>
             </h1>
-            <ul className="navbar">
-              <li className="navbar-item">
-                <a href="" className="navbar-item-active">首页</a>
-              </li>
-              <li className="navbar-item">
-                <a href="">博客文章</a>
-              </li>
-              <li className="navbar-item">
-                <a href="">技术分享</a>
-              </li>
-              <li className="navbar-item">
-                <a href="">关于我们</a>
-              </li>
-            </ul>
+            <nav className="navigation-pc">
+              <Link href='/' isActive={true} addHtml={false}>首页</Link>
+              <a href='/blog' title="小溪里博客">博客</a>
+              <a href='/hi-face' title="Hi头像教程">小册</a>
+              <Link href='/about' addHtml>关于</Link>
+            </nav>
             <div className="navigation-operation">
               <div></div>
               <div></div>
@@ -39,7 +41,7 @@ export default class Header extends React.Component {
             {/* 杠杠图标 */}
             <div 
               className={(menuState ? 'active' : '') + ' header-menu'}
-              onClick={() => this.changeMenuState(true)}
+              onClick={this.toggleMenuStatus}
             >
               <div></div>
               <div></div>
@@ -48,9 +50,9 @@ export default class Header extends React.Component {
         </header>
         {/* 导航菜单 */}
         <div
-          className={(menuState ? 'active' : '') + ' navigation'}
+          className={'navigation ' + (menuState ? 'active' : '')}
         >
-          <div className="navigation-menu" onClick={() => this.changeMenuState(false)}>
+          <div className="navigation-menu" onClick={this.toggleMenuStatus}>
             <div></div>
             <div></div>
           </div>

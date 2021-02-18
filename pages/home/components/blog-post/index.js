93c944e7a93384337998694a26798de28845300d
shenghanqin <shenghanqin@hujiang.com>
2021-02-18 15:21:13 +0800

    update 更新写法

--- pages/home/components/blog-post/index.js
+++ pages/home/components/blog-post/index.js
@@ -1,7 +1,36 @@
 import React from 'react'
 import ModuleTitle from '../../../../components/module-title'
-import { blogPostNews, blogPostList } from './service'
 import './styles.styl'
+
+const blogPostNews = {
+  pic: '/images/blog-post-news.png',
+  title: 'UI设计师与前端工程师不能错过的年度报告适配技巧',
+  info: '岁末年初，各大厂家都会推出各自的年度报告，这些年度报告有何特点呢？'
+}
+
+const blogPostList = [
+  {
+    id: 1,
+    month: 'SEP',
+    day: '23',
+    title: 'CSS3动画分享-积分签到',
+    info: '本文中会包含一个复合型动画，其项目背景为在 xx 平台签到时会赚取 C 币。'
+  },
+  {
+    id: 2,
+    month: 'SEP',
+    day: '23',
+    title: 'CSS3动画分享-积分签到',
+    info: '本文中会包含一个复合型动画，其项目背景为在 xx 平台签到时会赚取 C 币。'
+  },
+  {
+    id: 3,
+    month: 'SEP',
+    day: '23',
+    title: 'CSS3动画分享-积分签到',
+    info: '本文中会包含一个复合型动画，其项目背景为在 xx 平台签到时会赚取 C 币。'
+  }
+]
 
 export default class BlogPost extends React.PureComponent {
   render() {
